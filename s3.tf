52adbf061522b6634cfe43dc342bd76a6cc9095c
haeramkeem <ewqdsacxz2345@gmail.com>
2022-09-21 14:30:22 +0900

    feat: S3 buckets

--- s3.tf
+++ s3.tf
@@ -1,3 +1,7 @@
-resource "aws_s3_bucket" "elon-kiosk-statics" {
-  bucket = "elon-kiosk-statics"
+resource "aws_s3_bucket" "webview_bucket" {
+  bucket = "elon-kiosk-webview-bucket"
+}
+
+resource "aws_s3_bucket" "menu_img_bucket" {
+  bucket = "elon-kiosk-menu-img-bucket"
 }

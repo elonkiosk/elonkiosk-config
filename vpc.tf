7e8291ed8fe58890c4a8dd77ba3d8d21ac14e36b
haeramkeem <ewqdsacxz2345@gmail.com>
2022-10-04 16:13:09 +0900

    feat: public subnet setting

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,42 @@ resource "aws_vpc" "vpc_main" {
     Name = "elon-kiosk-vpc-main"
   }
 }
+
+# ----- Public Subnet -----
+resource "aws_subnet" "vpc_main_pub_subnet" {
+  vpc_id     = aws_vpc.vpc_main.id
+  cidr_block = "172.16.1.0/24"
+
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "elon-kiosk-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw_main" {
+  vpc_id = aws_vpc.vpc_main.id
+
+  tags = {
+    Name = "elon-kiosk-igw"
+  }
+}
+
+resource "aws_route_table" "vpc_main_pub_rtb" {
+  vpc_id = aws_vpc.vpc_main.id
+
+  tags = {
+    Name = "elon-kiosk-public-rtb"
+  }
+}
+
+resource "aws_route_table_association" "vpc_main_pub_rtb_assoc" {
+  subnet_id      = aws_subnet.vpc_main_pub_subnet.id
+  route_table_id = aws_route_table.vpc_main_pub_rtb.id
+}
+
+resource "aws_route" "pub_rtb_default" {
+  route_table_id              = aws_route_table.vpc_main_pub_rtb.id
+  destination_cidr_block      = "0.0.0.0/0"
+  gateway_id                  = aws_internet_gateway.igw_main.id
+}

befbe293e9852c65bd600eaff3ca56a80766e885
haeramkeem <ewqdsacxz2345@gmail.com>
2022-10-05 17:31:34 +0900

    style: terraform fmt

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket          = "admin-tfstate-bucket"
-    key             = "elon-kiosk-tfstate"
-    region          = "ap-northeast-2"
-    dynamodb_table  = "admin-tfstate-locking"
+    bucket         = "admin-tfstate-bucket"
+    key            = "elon-kiosk-tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "admin-tfstate-locking"
   }
 
   required_providers {
@@ -15,5 +15,5 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-northeast-2"
+  region = "ap-northeast-2"
 }

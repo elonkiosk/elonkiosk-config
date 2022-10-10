cbf93d336f37ecf3686287ba9acca68322bff937
haeramkeem <ewqdsacxz2345@gmail.com>
2022-10-11 03:02:33 +0900

    feat: Cert for ALB HTTPS connection

--- alb.tf
+++ alb.tf
@@ -102,29 +102,24 @@ resource "aws_alb_listener" "alb_main_listener_http" {
   protocol = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_alb_listener" "alb_main_listener_https" {
+  load_balancer_arn = aws_alb.alb_main.arn
+  port = 443
+  protocol = "HTTPS"
+  certificate_arn = aws_acm_certificate_validation.hz_main_cert_validate.certificate_arn
+
+  default_action {
     type = "forward"
     target_group_arn = aws_alb_target_group.alb_main_tg_api.arn
   }
-
-# TODO: TLS Cert setting
-#   default_action {
-#     type = "redirect"
-#
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-#
-# resource "aws_alb_listener" "alb_main_listener_https" {
-#   load_balancer_arn = aws_alb.alb_main.arn
-#   port = 443
-#   protocol = "HTTPS"
-#
-#   default_action {
-#     type = "forward"
-#     target_group_arn = aws_alb_target_group.alb_main_tg_api.arn
-#   }
-# }
 }

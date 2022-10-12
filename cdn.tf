5a93568e1a04c39ef78bcf36b1b3e695977fa673
haeramkeem <ewqdsacxz2345@gmail.com>
2022-10-12 16:11:35 +0900

    style: code structure

--- cdn.tf
+++ cdn.tf
@@ -25,7 +25,6 @@ resource "aws_cloudfront_distribution" "fe_distribution" {
   aliases = [format("elon-kiosk.%s", var.hz_main_name)]
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = "index.html"
   price_class = "PriceClass_200"
 
   restrictions {
@@ -39,27 +38,10 @@ resource "aws_cloudfront_distribution" "fe_distribution" {
     ssl_support_method  = "sni-only"
   }
 
-  # S3 Origin: customer webview bucket
-  origin {
-    domain_name = aws_s3_bucket.wv_customer_bucket.bucket_domain_name
-    origin_id   = aws_s3_bucket.wv_customer_bucket.id
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.fe_customer_oai.cloudfront_access_identity_path
-    }
-  }
-
-  # S3 Origin: store webview bucket
-  origin {
-    domain_name = aws_s3_bucket.wv_store_bucket.bucket_domain_name
-    origin_id   = aws_s3_bucket.wv_store_bucket.id
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.fe_store_oai.cloudfront_access_identity_path
-    }
-  }
-
-  # ALB origin
+  #############################################
+  # Precedence 0) ALB
+  #############################################
+  # Origin
   origin {
     domain_name = aws_alb.alb_main.dns_name
     origin_id   = aws_alb.alb_main.id
@@ -72,7 +54,7 @@ resource "aws_cloudfront_distribution" "fe_distribution" {
     }
   }
 
-  # ALB behavior (precedence 0)
+  # Behavior
   ordered_cache_behavior {
     path_pattern     = "/api*"
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -94,7 +76,20 @@ resource "aws_cloudfront_distribution" "fe_distribution" {
     viewer_protocol_policy = "redirect-to-https"
   }
 
-  # S3:store behavior (precedence default)
+  #############################################
+  # Precedence 1) S3:store webview bucket
+  #############################################
+  # Origin
+  origin {
+    domain_name = aws_s3_bucket.wv_store_bucket.bucket_domain_name
+    origin_id   = aws_s3_bucket.wv_store_bucket.id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.fe_store_oai.cloudfront_access_identity_path
+    }
+  }
+
+  # Behavior
   ordered_cache_behavior {
     path_pattern     = "/store*"
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -115,7 +110,22 @@ resource "aws_cloudfront_distribution" "fe_distribution" {
     max_ttl                = 86400
   }
 
-  # S3:customer behavior (default precedence)
+  #############################################
+  # Default precedence) S3:customer webview bucket
+  #############################################
+  default_root_object = "index.html"
+
+  # Origin
+  origin {
+    domain_name = aws_s3_bucket.wv_customer_bucket.bucket_domain_name
+    origin_id   = aws_s3_bucket.wv_customer_bucket.id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.fe_customer_oai.cloudfront_access_identity_path
+    }
+  }
+
+  # Behavior
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]

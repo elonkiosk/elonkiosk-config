cb507bb90ad9632e2bdf0d7e202f7dc1bdee5697
haeramkeem <ewqdsacxz2345@gmail.com>
2022-10-13 12:13:40 +0900

    feat: RDS SG

--- rds.tf
+++ rds.tf
@@ -1,3 +1,44 @@
+# SG
+resource "aws_security_group" "sg_db" {
+  name        = "elon-kiosk-db-sg"
+  description = "DB Security Group"
+  vpc_id      = aws_vpc.vpc_main.id
+
+  tags = {
+    Name = "elon-kiosk-db-sg"
+  }
+}
+
+## SG Rules
+### Ingress
+resource "aws_security_group_rule" "sg_db_rule_ing_conn" {
+  type                     = "ingress"
+  from_port = var.DB_PORT
+  to_port = var.DB_PORT
+  protocol                 = "TCP"
+  source_security_group_id        = aws_security_group.sg_apiserver.id
+  security_group_id = aws_security_group.sg_db.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+### Egress
+resource "aws_security_group_rule" "sg_db_rule_eg_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_db.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Subnet group
 resource "aws_db_subnet_group" "subnet_group_db" {
   name = "elon-kiosk-subnet-group-db"
   subnet_ids = [
@@ -10,6 +51,7 @@ resource "aws_db_subnet_group" "subnet_group_db" {
   }
 }
 
+# RDS instance
 resource "aws_db_instance" "db_main" {
   allocated_storage = 20
   max_allocated_storage = 50
@@ -24,4 +66,5 @@ resource "aws_db_instance" "db_main" {
   password = var.DB_PASSWORD
   db_name = var.DB_NAME
   port = var.DB_PORT
+  vpc_security_group_ids = [aws_security_group.sg_db.id]
 }
